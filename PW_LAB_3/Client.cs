61a7ea0005fe6e1dd1d2667663eb4590cd085f1c
pstosio <piotr.stosio08@gmail.com>
2015-12-21 11:27:15 +0100

    First commit- just connectionn

---
+++ PW_LAB_3/Client.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+
+namespace PW_LAB_3
+{
+    public partial class Client : Form
+    {
+        public Client()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string host = textBox1.Text;
+            int port = System.Convert.ToInt16(numericUpDown1.Value);
+            try
+            {
+                TcpClient klient = new TcpClient(host, port);
+                if (klient.Connected)
+                    listBox1.Items.Add("Nawiązano połączenie z " + host + " na porcie:" + port);
+
+                // Rozłączenie połączenia
+                klient.Close();
+            }
+            catch(Exception ex)
+            {
+                listBox1.Items.Add(String.Format("Błąd: Nie udało się nawiązać połączenia :(, bo: {0}", ex.ToString()));
+            }
+        }
+    }
+}
---
+++ PW_LAB_3_Server/Server.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+
+namespace PW_LAB_3_Server
+{
+    public partial class Server : Form
+    {
+        //pola prywatne
+        private TcpListener serwer;
+        private TcpClient klient;
+
+        public Server()
+        {
+            //CheckForIllegalCrossThreadCalls = false;// !!
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            IPAddress adresIP;
+            try
+            {
+                adresIP = IPAddress.Parse(textBox1.Text);
+            }
+            catch
+            {
+                listBox1.Items.Add("Błędny format adresu IP!");
+                textBox1.Text = String.Empty;
+                return;
+            }
+
+            int port = System.Convert.ToInt16(numericUpDown1.Value);
+            try
+            {
+                listBox1.Items.Add("Serwer uruchomiony, oczekuje na połączenie...");
+                button1.Enabled = false;
+
+                serwer = new TcpListener(adresIP, port);
+                serwer.Start();
+
+                serwer.BeginAcceptTcpClient(new AsyncCallback(AcceptTcpClientCallback), serwer);
+            }
+            catch (Exception ex)
+            {
+                listBox1.Items.Add(String.Format("Błąd inicjacji serwera: {0}", ex.ToString()));
+            }
+        }
+
+        private void AcceptTcpClientCallback(IAsyncResult asyncResult)
+        {
+            TcpListener s = (TcpListener)asyncResult.AsyncState;
+            klient = s.EndAcceptTcpClient(asyncResult);
+            SetListBoxText("Połączenie się powiodło!");
+
+            klient.Close();
+            serwer.Stop();
+        }
+
+        /// <summary>
+        /// Obsługa list Boxa z poziomu wątku
+        /// </summary>
+        /// <param name="tekst"></param>
+        private delegate void SetTextCallBack(string tekst);
+        private void SetListBoxText(string tekst)
+        {
+            if (listBox1.InvokeRequired)
+            {
+                SetTextCallBack f = new SetTextCallBack(SetListBoxText);
+                this.Invoke(f, new object[] { tekst });
+            }
+            else
+            {
+                listBox1.Items.Add(tekst);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serwer.Stop();
+                klient.Close();
+                listBox1.Items.Add("Zakończono pracę serwera ...");
+            }
+            catch (Exception ex)
+            {
+                listBox1.Items.Add(ex.ToString());
+            }
+        }
+    }
+}
